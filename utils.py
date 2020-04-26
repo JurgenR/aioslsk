4ebabbacf0df875c412fb800c8c4618b6acf0cd1
JurgenR <1249228+JurgenR@users.noreply.github.com>
2020-04-26 20:45:45 +0200

    Utilities for retrieving local file information

--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
+import os
+
+
 def ticket_generator():
     for idx in range(1111, 0xFFFFFFFF):
         yield idx
+
+
+def get_directories_absolute_paths(directories):
+    abs_paths = []
+    for directory in directories:
+        abs_paths.append(os.path.abspath(directory))
+    return abs_paths
+
+
+def get_file_count(directory):
+    count = 0
+    for root, dirs, files in os.walk(directory):
+        count += len(files)
+    return count
