baaa482b2cc0b5adcd22dbe2ebdb11a7df67e238
JurgenR <1249228+JurgenR@users.noreply.github.com>
2022-01-02 23:27:03 +0100

    fix reference in exceptions

--- exceptions.py
+++ exceptions.py
@@ -7,4 +7,4 @@ class UnknownMessageError(PySlskException):
     def __init__(self, message_id: bytes, data: int, message):
         self.message_id = message_id
         self.data = data
-        self.message
+        self.message = message
