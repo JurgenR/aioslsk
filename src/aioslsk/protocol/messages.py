cacf84aae8911ecf83d6a29285da2fa237f78a18
JurgenR <1249228+JurgenR@users.noreply.github.com>
2026-09-21 18:26:53 +0200

    feat: use unique version instead of impersonating SoulSeekQt (#542)
    * feat: use major version 175
    
    * feat: incorporate feedback

--- src/aioslsk/protocol/messages.py
+++ src/aioslsk/protocol/messages.py
@@ -125,10 +125,14 @@ class Login(ServerMessage):
     * The ``md5hash`` parameter in the response is the MD5 hash of the
       ``password``
 
-    .. note::
+    aioslsk uses reserved client version 175. Minor version could be modified
+    if significant changes are made to network capabilities
 
-        Older client versions (at least 149 or below) would not send the
-        ``md5hash`` and ``minor_version``
+    Older client versions (at least 149 or below) would not send the
+    ``md5hash`` and ``minor_version``
+
+    More information on versions can found in the
+    `Nicotine+ documentation <https://nicotine-plus.org/doc/SLSKPROTOCOL.html>`_
 
     :status: USED
     """
